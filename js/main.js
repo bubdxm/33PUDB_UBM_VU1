f29d1cabbb60f9f741e9275eb5e9620c4f9c378d
symac <web@geobib.fr>
2019-08-28 09:00:35 +0200

    Amélioration du système de rebond vers d'autres bases en reprenant les modifications d'Alexandre sur BxSA.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,12 @@
-var app = angular.module('viewCustom', []);
-app.value('counter', 1);
+let app = angular.module('viewCustom',[]);
 
 app.component('prmSearchBarAfter', {
   bindings: { parentCtrl: '<' },
   templateUrl: 'custom/33PUDB_UBM_VU1/html/prmSearchBarAfter.html'
 });
 
-app.component('prmBriefResultContainerAfter', {
-  bindings: { parentCtrl: '<' },
-  controller: ['$scope', '$rootScope', '$element', function controller($scope, $rootScope, $element) {
-    if (typeof $rootScope.counter !== "undefined") {
-      $rootScope.counter = $rootScope.counter + 1;
-    } else {
-      $rootScope.counter = 1;
-    }
+import {linkToExternalSearchSystemConfig} from './prmBriefResultContainerAfter/linkToExternalSearchSystem'
 
-    var resultList = angular.element(document.getElementsByClassName('list-item-wrapper'));
-    var resultCount = resultList.length;
-
-    if ( (resultCount <= 10) && ( $rootScope.counter == resultCount) ) {
-      $scope.showExternalSiteList = true;
-    } else if ($rootScope.counter == 10) {
-      $scope.showExternalSiteList = true;
-    }
-
-    console.log("CONTER : " + $rootScope.counter + " / " + "resultCount : " + resultCount);
-
-    $scope.queryTerm = $scope.$parent.$ctrl.searchService.searchFieldsService._mainSearch;
-  }],
-  templateUrl: 'custom/33PUDB_UBM_VU1/html/linkExternalSiteResultList.html'
-});
+if (app){
+    app.component('prmBriefResultContainerAfter', linkToExternalSearchSystemConfig);
+} 
---
+++ js/prmBriefResultContainerAfter/linkToExternalSearchSystem.js
@@ -0,0 +1,38 @@
+import linkToExternalSearchSystem from '../../html/linkExternalSiteResultList.html'
+
+class linkToExternalSearchSystemController {
+    constructor($scope, $window, favSession){
+        console.log(this.parentCtrl.storageUtil.localStorage.testLocal)
+        console.log('----> 33PUDB linkToExternalSearchSystemController');
+        var lastResult = this.parentCtrl.searchStateService.resultObject.info.last;
+        var resultIndex = this.parentCtrl.index;
+        console.log(this.parentCtrl.searchStateService.resultObject.info);
+        console.log(this);
+        if (resultIndex == lastResult && this.parentCtrl.isExpanded == false) {
+          // this.setData('true');
+          if (this.parentCtrl.storageUtil.localStorage.getItem('viewExternalSearchMessage') != 'false'){
+            this.showExternalSiteList = true;
+            this.parentCtrl.storageUtil.localStorage.setItem('viewExternalSearchMessage','true');
+          }
+        } 
+
+        console.log("COUNTER2 : " + resultIndex + " / "  + lastResult);
+    
+        $scope.queryTerm = encodeURIComponent(this.parentCtrl.searchService.searchFieldsService._mainSearch);
+    }
+
+    externalSearchMsgOnClick (){
+      console.log("It's time to kick ass and chew bubble gum, and I'm all outta gum!");
+      this.showExternalSiteList = false;
+      this.parentCtrl.storageUtil.localStorage.setItem('viewExternalSearchMessage','false');
+    };
+  
+  }
+
+
+  linkToExternalSearchSystemController.$inject = ['$scope', '$rootScope', '$element']
+  export let linkToExternalSearchSystemConfig = {
+    bindings: {parentCtrl:'<'},
+    controller: linkToExternalSearchSystemController,
+    template:  linkToExternalSearchSystem,
+  }
